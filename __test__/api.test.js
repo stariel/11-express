a08772b76a24d12f02dcef68089c90d86a1defb5
Ariel Altaras <ariel.altaras@gmail.com>
2018-06-16 17:30:08 -0700

    Travis troubleshooting

--- __test__/api.test.js
+++ __test__/api.test.js
@@ -6,7 +6,7 @@ describe('Express Web Server', () => {
 
   it('handles an invalid get request with a 404', () => {
 
-    return superagent.get('http://localhost:3000/blah')
+    return superagent.get('http://localhost:3000/api/v1/cats/blah')
       .then(true)
       .catch(response => expect(response.status).toEqual(404));
 
